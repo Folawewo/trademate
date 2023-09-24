d1d61d41ee2555565032043ed7cb69d80611c94a
Folawewo <famwallexie@gmail.com>
2023-09-24 19:59:43 +0100

    stocks

--- Controllers/StocksController.cs
+++ Controllers/StocksController.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using trademate.Data;
+using trademate.Models;
 
 namespace trademate.Controllers
 {
@@ -11,5 +14,91 @@ namespace trademate.Controllers
     [ApiController]
     public class StocksController : ControllerBase
     {
+        private readonly ApplicationDbContext _context;
+
+        public StocksController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // api/stocks
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Stock>>> GetStocks()
+        {
+            return await _context.Stocks.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Stock>> GetStock(int id)
+        {
+            var stock = await _context.Stocks.FindAsync(id);
+
+            if (stock == null)
+            {
+                return NotFound();
+            }
+
+            return stock;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Stock>> PostStock(Stock stock)
+        {
+            _context.Stocks.Add(stock);
+
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetStock), new { id = stock.Id }, stock);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Stock>> PutStock(int id, Stock stock)
+        {
+            if (id != stock.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(stock).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!StockExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteStock(int id)
+        {
+            var stock = await _context.Stocks.FindAsync(id);
+            if (stock == null)
+            {
+                return NotFound();
+            }
+
+            _context.Stocks.Remove(stock);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool StockExists (int id)
+        {
+            return _context.Stocks.Any(e => e.Id == id);
+        }
     }
 }
